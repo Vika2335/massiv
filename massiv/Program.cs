5766755c4683601d682f009b662660b65d4ed038
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-02-07 21:07:58 +0300

    Добавьте файлы проекта.

---
+++ massiv/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Столбцов: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Строк: ");
+        int m = int.Parse(Console.ReadLine());
+        int[,] mas = new int[n, m];
+        Console.WriteLine();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write("Введите элементы массива: ");
+                mas[i, j] = int.Parse(Console.ReadLine());;
+            }
+        }
+        Console.WriteLine();
+
+        int result = 0;
+        int[] mas1 = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (mas[j, i] % 2 == 0 && result == 0)
+                {
+                    result = mas[j, i];
+                }
+            }
+            mas1[i] = result;
+            result = 0;
+        }
+        for (int i = 0; i < mas1.Length; i++)
+        {
+            Console.WriteLine($"{mas1[i]} \t");
+        }   
+    }
+}
